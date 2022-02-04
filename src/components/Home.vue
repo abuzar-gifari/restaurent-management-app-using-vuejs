39ae019a5f799c38ce9ec02501b4d2ab31197f24
abuzar-gifari <shaikh35-3024@diu.edu.bd>
2022-02-04 17:06:55 +0600

    restaurent list found using get api

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,27 @@
 <template>
   <Header/>
   <h1>Hello {{ name }} welcome to home page</h1>
+
+  <table border="1">
+    <tr>
+      <td>ID</td>
+      <td>NAME</td>
+      <td>CONTACT</td>
+      <td>ADDRESS</td>
+    </tr>
+    <tr v-for="item in restaurant" :key="item.id">
+      <td>{{ item.id }}</td>
+      <td>{{ item.name }}</td>
+      <td>{{ item.contact }}</td>
+      <td>{{ item.address }}</td>
+    </tr>
+  </table>
+
 </template>
 
 <script>
 import Header from "./Header.vue"
+import axios from "axios"
 export default {
   name: 'Home',
   components:{
@@ -12,19 +29,27 @@ export default {
   },
   data(){
     return{
-      name:""
+      name:"",
+      restaurant:[]
     }
   },
-  mounted() {
+  async mounted() {
     let user=localStorage.getItem("user-info");
     this.name=JSON.parse(user).name;
     if (!user){
       this.$router.push({ name:"SignUp" });
     }
+
+    let result = await axios.get("http://localhost:3000/restaurent");
+    console.warn(result);
+    this.restaurant=result.data;
   }
 }
 </script>
 
 <style>
-
+td{
+  width:140px;
+  height: auto;
+}
 </style>
